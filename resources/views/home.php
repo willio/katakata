5cc6e22bc38a83c3a1896cf20a19094b5221aa0f
Will W <860569+willio@users.noreply.github.com>
2026-07-27 23:43:58 +0700

    feat: add plain PHP view foundation

---
+++ resources/views/home.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= e($name) ?></title>
+</head>
+<body>
+    <main>
+        <h1><?= e($name) ?></h1>
+        <p><?= e($tagline) ?></p>
+    </main>
+</body>
+</html>
