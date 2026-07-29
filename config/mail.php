3fa7817c2c675b06449091f74c2f554a5f0d002c
Will W <860569+willio@users.noreply.github.com>
2026-07-30 00:52:57 +0700

    config: add Resend webhook secret

--- config/mail.php
+++ config/mail.php
@@ -6,4 +6,5 @@ return [
     'transport' => env('MAIL_TRANSPORT', 'filesystem'),
     'from' => env('MAIL_FROM', ''),
     'resend_key' => env('RESEND_API_KEY', ''),
+    'resend_webhook_secret' => env('RESEND_WEBHOOK_SECRET', ''),
 ];
