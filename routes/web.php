e914f47e6536e71d7ccea03a19f03250b73a5771
Will W <860569+willio@users.noreply.github.com>
2026-07-30 08:01:31 +0700

    feat(home): supply reader homepage content

--- routes/web.php
+++ routes/web.php
@@ -36,9 +36,23 @@ $recordVisit = static function (Request $request) use ($app): void {
 
 $router->get('/', function (Request $request) use ($app, $recordVisit): Response {
     $recordVisit($request);
+    $repository = $app->make(Repository::class);
+    $posts = $app->make(Home::class)->latest($repository->posts());
+    $authors = [];
+
+    foreach ($posts as $post) {
+        if ($post->author !== null) {
+            $authors[$post->slug] = $repository->findAuthor($post->author);
+        }
+    }
+
     return Response::html($app->make(View::class)->render('home', [
         'name' => (string) $app->config()->get('app.name', 'Katakata'),
         'tagline' => (string) $app->config()->get('app.tagline', ''),
+        'siteUrl' => rtrim((string) $app->config()->get('app.url', 'http://localhost:8000'), '/'),
+        'posts' => $posts,
+        'authors' => $authors,
+        'csrf' => $app->make(Session::class)->csrf(),
     ]));
 });
 
