55e74a23ca76bbb7adfc3cdb87056b391aaf091b
Will W <860569+willio@users.noreply.github.com>
2026-08-03 09:49:04 +0700

    test(mail): update standalone sync coordinator bootstrap contract

--- tests/Feature/MailSyncJobBootstrapTest.php
+++ tests/Feature/MailSyncJobBootstrapTest.php
@@ -8,16 +8,18 @@ use PHPUnit\Framework\TestCase;
 
 final class MailSyncJobBootstrapTest extends TestCase
 {
-    public function testStandaloneSyncJobLoadsMailBindingsBeforeResolvingSynchronizer(): void
+    public function testStandaloneSyncJobLoadsMailBindingsBeforeResolvingCoordinator(): void
     {
         $job = file_get_contents(dirname(__DIR__, 2) . '/private/jobs/sync-mail.php');
 
         self::assertIsString($job);
         $mailBootstrap = strpos($job, "require dirname(__DIR__, 2) . '/bootstrap/mail.php';");
-        $resolution = strpos($job, '$app->make(ImapSynchronizer::class)');
+        $resolution = strpos($job, '$app->make(MailboxSyncCoordinator::class)');
 
         self::assertNotFalse($mailBootstrap);
         self::assertNotFalse($resolution);
         self::assertLessThan($resolution, $mailBootstrap);
+        self::assertStringContainsString('use Katakata\\Email\\MailboxSyncCoordinator;', $job);
+        self::assertStringNotContainsString('$app->make(ImapSynchronizer::class)', $job);
     }
 }
